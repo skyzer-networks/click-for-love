3bf60c2bb7c7131c199079b95b0addc924f36f53
michaelyurachek <michaelyurachek@gmail.com>
2026-02-02 22:47:37 -0500

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/click-for-love/",
   plugins: [react()],
-  base: '/click-for-love/', // <-- MUST be top-level
-  server: {
-    host: true,
-    https: true,
-  },
 })
